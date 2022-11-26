9e2d79acf04a0fa3f88c837829e41c4615d66a61
Samira Shajahan Borsha <57401425+Samira-Shajahan-Borsha@users.noreply.github.com>
2022-11-26 17:41:14 +0600

    input field validate

--- Js/script.js
+++ Js/script.js
@@ -1,26 +1,39 @@
+//button
 const calculateBtn = document.getElementById('calculate-btn');
 
-calculateBtn.addEventListener('click', function () {
+//get input value
+function getInputValue(inputId) {
 
-    const incomeInputText = document.getElementById('income-input');
+    const inputText = document.getElementById(inputId);
+
+    const inputValue = parseInt(inputText.value);
+
+    inputText.value = '';
+    return inputValue;
+
+
+}
+//calculate total expenses and balance
+function calculate() {
+
     const totalExpenseField = document.getElementById('total-expenses');
     const totalBalanceField = document.getElementById('total-balance');
 
-    const foodInputText = document.getElementById('food-input');
-    const rentInputText = document.getElementById('rent-input');
-    const clothesInputText = document.getElementById('clothes-input');
+    const totalIncome = getInputValue('income-input');
 
-    const totalIncome = parseInt(incomeInputText.value);
-    const foodExpense = parseFloat(foodInputText.value);
-    const rentExpense = parseFloat(rentInputText.value);
-    const clothesExpense = parseFloat(clothesInputText.value);
-
+    const foodExpense = getInputValue('food-input');
+    const rentExpense = getInputValue('rent-input');
+    const clothesExpense = getInputValue('clothes-input');
 
     const totalExpenseAmount = foodExpense + rentExpense + clothesExpense;
-    totalExpenseField.innerText = totalExpenseAmount.toFixed(1);
+    totalExpenseField.innerText = totalExpenseAmount;
 
-    
     const balance = totalIncome - totalExpenseAmount;
     totalBalanceField.innerText = balance;
 
+
+}
+
+calculateBtn.addEventListener('click', function () {
+    calculate();
 });
