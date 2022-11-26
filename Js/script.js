c6641ff788c6d962a9a7c6b8c2b928dbd6d3c15b
Samira Shajahan Borsha <57401425+Samira-Shajahan-Borsha@users.noreply.github.com>
2022-11-26 18:55:26 +0600

    saving & remaining amount calculated

--- Js/script.js
+++ Js/script.js
@@ -1,5 +1,5 @@
-//button
 const calculateBtn = document.getElementById('calculate-btn');
+const saveBtn = document.getElementById('save-btn');
 
 //get input value
 function getInputValue(inputId) {
@@ -11,29 +11,56 @@ function getInputValue(inputId) {
     inputText.value = '';
     return inputValue;
 
-
 }
+
 //calculate total expenses and balance
 function calculate() {
-
+    const incomeInput = document.getElementById('income-input');
     const totalExpenseField = document.getElementById('total-expenses');
     const totalBalanceField = document.getElementById('total-balance');
 
-    const totalIncome = getInputValue('income-input');
+    const totalIncome = parseInt(incomeInput.value); 
 
     const foodExpense = getInputValue('food-input');
     const rentExpense = getInputValue('rent-input');
     const clothesExpense = getInputValue('clothes-input');
 
-    const totalExpenseAmount = foodExpense + rentExpense + clothesExpense;
-    totalExpenseField.innerText = totalExpenseAmount;
+    if (isNaN(totalIncome) || isNaN(foodExpense) || isNaN(rentExpense) || isNaN(clothesExpense)) {
+        return alert("You are required to enter a valid number");
+    }
 
-    const balance = totalIncome - totalExpenseAmount;
-    totalBalanceField.innerText = balance;
+    else if (totalIncome < 0 || foodExpense < 0 || rentExpense < 0 || clothesExpense < 0) {
+        return alert("You are required to enter a positive number");
+    }
 
+    else {
+        const totalExpenseAmount = foodExpense + rentExpense + clothesExpense;
+        totalExpenseField.innerText = totalExpenseAmount;
+
+        const balance = totalIncome - totalExpenseAmount;
+        totalBalanceField.innerText = balance;
+    }
 
 }
 
 calculateBtn.addEventListener('click', function () {
     calculate();
+});
+
+
+
+saveBtn.addEventListener('click', function () {
+    const totalIncome = getInputValue('income-input');
+    const percentageOfSavingAmount = getInputValue('save-input');
+    const savingAmountField = document.getElementById('saving-amount');
+    const remainingBalance = document.getElementById('remaining-balance');
+    const totalBalanceField = document.getElementById('total-balance');
+
+    if (percentageOfSavingAmount > 0) {
+        const savingAmount = totalIncome * 0.2;
+        savingAmountField.innerText = savingAmount;
+
+        remainingBalance.innerText = parseInt(totalBalanceField.innerText) - savingAmount;
+    }
+
 });
